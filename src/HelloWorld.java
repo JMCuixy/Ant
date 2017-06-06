b926897502afef9e0adc738cde98ae26781b50e5
崔修印 <1099442418@qq.com>
2017-06-06 10:21:09 +0800

    初始化仓库

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/6/5.
+ * <p>
+ * Description:
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello,World");
+    }
+}
